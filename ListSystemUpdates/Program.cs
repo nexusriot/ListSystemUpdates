fca28a671cbf1e6938b4908edd2aaf2a4fdfe834
Vlad Ananyev <vlananyev@gmail.com>
2020-10-31 17:39:09 +0300

    Initial commit

---
+++ ListSystemUpdates/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Management;
+using System.Diagnostics;
+using System.IO;
+using WUApiLib;
+using System.Reflection;
+using Microsoft.Win32;
+using System.Security.Principal;
+
+namespace ListSystemUpdates
+{
+
+    class Program
+    {
+
+        private static void AdminRelauncher()
+        {
+            if (!IsRunAsAdmin())
+            {
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                proc.FileName = Assembly.GetEntryAssembly().CodeBase;
+                proc.Verb = "runas";
+                try
+                {
+                    Process.Start(proc);
+                    Process.GetCurrentProcess().Kill();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("This program must be run as an administrator! \n\n" + ex.ToString());
+                }
+            }
+        }
+
+        private static bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private static void InstalledUpdatesFallback()
+        {
+            const string query = "SELECT HotFixID FROM Win32_QuickFixEngineering";
+            var search = new ManagementObjectSearcher(query);
+            var collection = search.Get();
+
+            foreach (ManagementObject quickFix in collection)
+                Console.WriteLine(quickFix["HotFixID"].ToString());
+        }
+
+        private static string GetMachineName()
+        {
+            return Environment.MachineName;
+    }
+
+        private static void InstalledUpdates()
+        {
+            UpdateSession UpdateSession = new UpdateSession();
+            IUpdateSearcher UpdateSearchResult = UpdateSession.CreateUpdateSearcher();
+            UpdateSearchResult.Online = false;
+            ISearchResult SearchResults = UpdateSearchResult.Search("IsInstalled=1 AND IsHidden=0");
+            //http://msdn.microsoft.com/en-us/library/windows/desktop/aa386526(v=VS.85).aspx
+             foreach (IUpdate x in SearchResults.Updates)
+            {
+                Console.WriteLine($"{x.Title} issued {x.LastDeploymentChangeTime}");
+            }
+        }
+        private static string GetOSVersionString()
+        {
+            var os = Environment.OSVersion;
+            return os.VersionString;
+        }
+
+        private static string GetReleaseId()
+        {
+            return Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ReleaseId", "").ToString();
+        }
+
+        private static string GetEditionId()
+        {
+            return Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "EditionId", "").ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            AdminRelauncher();
+            Console.WriteLine("Collecting, please wait. Result will be saved in Result.txt");
+            Console.WriteLine("Collecting installed Hot fixes");
+            FileStream fs = new FileStream("Result.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            Console.SetOut(sw);
+            Console.WriteLine("--------Windows info--------");
+            Console.WriteLine($"Machine name: {GetMachineName()}");
+            Console.Write($"{GetOSVersionString()} {GetEditionId()}");
+            var releaseId = GetReleaseId();
+            if (!string.IsNullOrEmpty(releaseId))
+                Console.WriteLine($"Release Id: {releaseId}");
+            else Console.WriteLine();
+                Console.WriteLine("--------Windows updates list--------");
+            try
+            {
+                InstalledUpdates();
+            }      
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to fetch updates via WUApi because of {ex.Message}");
+                Console.WriteLine("Getting KB list using WMI");
+                InstalledUpdatesFallback();
+            }    
+            sw.Close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+### ListSystemUpdates
+
+Simple utility to get list of installed Windows updates and hotfixes.
+
+example output
+```
+--------Windows info--------
+Machine name: WINDOWS-DEV
+Microsoft Windows NT 6.2.9200.0 Professional
+--------Windows updates list--------
+Update for Microsoft Camera Codec Pack for Windows 8.1 for x64-based Systems (KB2899189) issued 12/10/2013 12:00:00 AM
+Security Update for Windows 8.1 for x64-based Systems (KB2962140) issued 5/5/2014 12:00:00 AM
+Update for .NET Native on Windows 8.1 and Windows Server 2012 R2 for x64-based Systems (KB2954879) issued 5/13/2014 12:00:00 AM
+Update for Windows 8.1 for x64-based Systems (KB2968599) issued 6/10/2014 12:00:00 AM
+Update for Windows 8.1 for x64-based Systems (KB3126030) issued 3/8/2016 12:00:00 AM
+Security Update for Windows 8.1 for x64-based Systems (KB3139398) issued 3/8/2016 12:00:00 AM
+Security Update for Windows 8.1 for x64-based Systems (KB3139914) issued 3/8/2016 12:00:00 AM
+Cumulative Security Update for Internet Explorer 11 for Windows 8.1 for x64-based Systems (KB3185319) issued 9/13/2016 12:00:00 AM
+Security Update for Windows 8.1 for x64-based Systems (KB3187754) issued 9/13/2016 12:00:00 AM
+Update for Windows 8.1 for x64-based Systems (KB3184143) issued 10/11/2016 12:00:00 AM
+Update for Windows 8.1 for x64-based Systems (KB3173424) issued 5/9/2017 12:00:00 AM
+2017-09 Security and Quality Rollup for .NET Framework 3.5, 4.5.2, 4.6, 4.6.1, 4.6.2, 4.7 on Windows 8.1 and Server 2012 R2 for x64 (KB4041085) issued 9/12/2017 12:00:00 AM
+2017-11 Security and Quality Rollup for .NET Framework 3.5, 4.5.2, 4.6, 4.6.1, 4.6.2, 4.7 on Windows 8.1 and Server 2012 R2 for x64 (KB4049017) issued 11/27/2017 12:00:00 AM
+Update for Windows 8.1 for x64-based Systems (KB2976978) issued 7/10/2018 12:00:00 AM
+Windows Malicious Software Removal Tool x64 - v5.82 (KB890830) issued 5/12/2020 12:00:00 AM
+2020-06 Security Update for Adobe Flash Player for Windows 8.1 for x64-based Systems (KB4561600) issued 6/9/2020 12:00:00 AM
+2020-08 Security and Quality Rollup for .NET Framework 3.5, 4.5.2, 4.6, 4.6.1, 4.6.2, 4.7, 4.7.1, 4.7.2, 4.8 for Windows 8.1 and Server 2012 R2 for x64 (KB4570508) issued 8/11/2020 12:00:00 AM
+2020-08 Security Monthly Quality Rollup for Windows 8.1 for x64-based Systems (KB4571703) issued 8/11/2020 12:00:00 AM
+```
